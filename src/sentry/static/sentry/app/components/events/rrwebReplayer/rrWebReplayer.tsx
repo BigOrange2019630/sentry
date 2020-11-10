3a6f3624b600e426389a829768ede10fd7e38520
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-11-10 16:59:52 +0100

    update

--- src/sentry/static/sentry/app/components/events/rrwebReplayer/rrWebReplayer.tsx
+++ src/sentry/static/sentry/app/components/events/rrwebReplayer/rrWebReplayer.tsx
@@ -2,12 +2,29 @@ import React from 'react';
 import rrwebPlayer from 'rrweb-player';
 import * as Sentry from '@sentry/react';
 
+import {t} from 'app/locale';
+import {Client} from 'app/api';
+import LoadingError from 'app/components/loadingError';
+import LoadingIndicator from 'app/components/loadingIndicator';
+import {Panel} from 'app/components/panels';
+import withApi from 'app/utils/withApi';
+
 type Props = {
+  api: Client;
   url: string;
   className?: string;
 };
 
-class RRWebReplayer extends React.Component<Props> {
+type State = {
+  isLoading: boolean;
+  hasError: boolean;
+};
+
+class RRWebReplayer extends React.Component<Props, State> {
+  state: State = {
+    isLoading: true,
+    hasError: false,
+  };
   componentDidMount() {
     this.rrwebPlayer();
   }
@@ -16,32 +33,68 @@ class RRWebReplayer extends React.Component<Props> {
 
   newRRWebPlayer: any;
 
-  async rrwebPlayer() {
+  rrwebPlayer = async () => {
+    this.setState({isLoading: true, hasError: false});
+
     const element = this.wrapperRef?.current;
 
     if (!element) {
       return;
     }
 
-    const {url} = this.props;
+    const {url, api} = this.props;
 
     try {
-      const resp = await fetch(url);
+      const resp = await api.requestPromise(url);
       const payload = await resp.json();
       this.newRRWebPlayer = new rrwebPlayer({
         target: element,
         autoplay: false,
         data: payload,
       });
+      this.setState({isLoading: false});
     } catch (err) {
       Sentry.captureException(err);
+      this.setState({isLoading: false, hasError: true});
     }
+  };
+
+  renderSubContent() {
+    const {hasError, isLoading} = this.state;
+
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
+
+    if (hasError) {
+      return (
+        <LoadingError
+          message={t('There was a problem loading the player')}
+          onRetry={this.rrwebPlayer}
+        />
+      );
+    }
+
+    return null;
   }
 
   render() {
     const {className} = this.props;
-    return <div ref={this.wrapperRef} className={className} />;
+
+    const subContent = this.renderSubContent();
+
+    const content = (
+      <div ref={this.wrapperRef} className={className}>
+        {subContent}
+      </div>
+    );
+
+    if (!subContent) {
+      return <Panel>{content}</Panel>;
+    }
+
+    return content;
   }
 }
 
-export default RRWebReplayer;
+export default withApi(RRWebReplayer);
